262fa9f90222636e6f8d8ca1bb7ec1a2faa12a82
Edwin Cocha <edwin.cocha@epn.edu.ec>
2021-02-17 11:00:13 -0500

    clase nombre

--- clases/src/app.controller.ts
+++ clases/src/app.controller.ts
@@ -10,3 +10,53 @@ export class AppController {
     return this.appService.getHello();
   }
 }
+
+abstract class Nombre {
+  public nombrePropiedad?: string; // undefined
+  private apellidoPropiedad: string = 'Eguez';
+  protected edad = 1; // number (Duck Typing)
+  static comun: number = 10;
+
+
+
+  propiedadPublica: string;
+
+
+
+  constructor(
+      propiedadPublicaParametro: string, // parametro
+      public propiedadRapido: string, // transforma una propiedad
+  ) {
+    this.propiedadPublica = propiedadPublicaParametro;
+    // this.propiedadPublicaParametro ERROR
+    // this.propiedadRapido; OK
+  }
+
+
+
+  public funcionPublica(parametroString: string): void {
+    // no hay return = undefined
+  }
+
+
+
+  private funcionPrivada(parametroString: string,
+                         parametroNumber: number) { // omitir :void (defecto)
+    // no hay return = undefined
+  }
+
+
+
+  protected funcionPublica1(): number {
+    return 1;
+  }
+
+
+
+  static funcionPublica(): string {
+    return 'string';
+  }
+
+
+
+}
